c68257d53f9c164448f8eebbab0364fa66abca1e
ms-ask <ms.asck@yandex.ru>
2022-10-12 19:51:06 +0300

    feat: add Swagger

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,20 +16,32 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "Users", description = "управление пользователями")
 public class UserController {
 
     private Storage<User> storage = new Storage();
 
+    @Operation(
+            summary = "Получение списка всех пользователей"
+    )
     @GetMapping
     public List<User> getAllUsers() {
         return storage.getAllItems();
     }
 
+    @Operation(
+            summary = "Добавление пользователя",
+            description = "нет проверки на уникальность"
+    )
     @PostMapping
     public User addUser(@RequestBody @Valid User user) {
         return storage.add(user);
     }
 
+    @Operation(
+            summary = "Обновление пользователя",
+            description = "для существующих пользователей"
+    )
     @PutMapping
     public User updateUser(@RequestBody @Valid User user) {
         return storage.update(user);
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import ru.yandex.practicum.filmorate.helper.ReleaseDate;
@@ -12,13 +13,20 @@ import java.time.LocalDate;
 @Data
 @RequiredArgsConstructor
 public class Film implements IdControl {
+
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     private int id;
     @NotBlank(message = "'name' should non be empty")
     private String name;
     @Size(max = 200, message = "'description' should have length no more than 200")
     private String description;
+
+    @Schema(description = "Дата выхода. Старше 28 декабря 1895 года")
     @ReleaseDate
     private LocalDate releaseDate;
+    @Schema(
+            description = "Продолжительность (в секундах). Больше нуля",
+    example = "9000")
     @Positive
     private int duration;
 
